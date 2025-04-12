444c07a3b2ca0298a9ee9148056c31d8bf21ceed
Vedant <vedantbumrela1125@gmail.com>
2025-04-12 11:00:35 +0530

    reqirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vedant",
+    author_email="vedantbumrela1125@gmail.com",
+    packages=find_packages()
+)
